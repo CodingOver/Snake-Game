69c9487a6d41c645127434d3e6801e9122127571
CodingOver <paypal1business.over@gmail.com>
2023-02-11 22:14:06 +0100

    Add Keys Controllers

--- main.js
+++ main.js
@@ -125,3 +125,36 @@ let helpers = {
         return start * (1 - t) + end * t;
     }
 }
+
+// Keys
+
+let KEY = {
+    ArrowUp: false,
+    ArrowRight: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    resetState() {
+        this.ArrowUp = false;
+        this.ArrowRight = false;
+        this.ArrowDown = false;
+        this.ArrowLeft = false;
+    },
+    listen() {
+        addEventListener("keydown", () => {
+            (e) => {
+                if (e.key === "ArrowUp" && this.ArrowDown) return;
+                if (e.key === "ArrowDown" && this.ArrowUp) return;
+                if (e.key === "ArrowLeft" && this.ArrowRight) return;
+                if (e.key === "ArrowRight" && this.ArrowLeft) return;
+
+                this[e.key] = true;
+                Object.keys(this)
+                    .filter((f) => !== e.keys && f !== "listen" && f !== "resetState")
+                    .forEach((k) => {
+                        this[k] = false;
+                    });
+            },
+                false
+        })
+    }
+}
